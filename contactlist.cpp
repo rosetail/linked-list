f186d6c65f4de44dfb05d4b112ba5d8bf96ba2ca
Rose Osterheld <dev@roeli.org>
2021-03-07 23:38:02 -0800

    Implement constructor and print()
    Canvas says that we have a file named contactlist.cpp, but it also
    alludes to us implementing a phonebook class. All of the methods in
    contact.hpp are defined and empty (meaning that they end in {} and not
    ;), so it is impossible to override them and make them actually do
    something. What I've done is put a class called PhoneBook in
    contactList.cpp that extends the List class in
    contact.hpp. Unfortunately, head is private in the List class, so
    PhoneBook needs it's own head node. This means we're literally redoing
    everything that's in the List class. This isn't very elegant, but I
    don't see any other way to follow all of the directions. If you have
    any ideas on how to improve this, they are welcome.
    
    Also, @Maren, I think we have to put both of our work in this
    file. I've already added you as an author, and you're welcome to add
    your methods to the class definition.

--- contactlist.cpp
+++ contactlist.cpp
@@ -1,1 +1,34 @@
-// contactlist
+/**
+ * CIS 22B
+ *
+ * Lab4: A contact list that is implemented using a linked list
+ *
+ * Author: Rose Osterheld, Maren Alessandri Lane
+ *
+ * Group 4: Rose Osterheld, Maren Alessandri Lane, Alex Hagemeister
+
+ *
+ * Date: 03/07/2021
+ */
+
+#include "contact.hpp"
+#include <iostream>
+
+class PhoneBook : List
+{
+  private:
+    Node *head;
+
+  public:
+    PhoneBook() : head(nullptr) {}
+
+    void print() const
+    {
+        Node *currentNode = head;
+        while (currentNode != nullptr)
+        {
+            cout << currentNode->data;
+            currentNode = currentNode->next;
+        }
+    }
+};
