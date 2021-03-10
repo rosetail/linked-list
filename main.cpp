7a40e1f4e7a8c51fa2b727936455e88f852bad5f
Rose Osterheld <dev@roeli.org>
2021-03-09 16:27:50 -0800

    Make main call remove with a Contact, instead of fName and lName

--- main.cpp
+++ main.cpp
@@ -11,72 +11,82 @@
  * Date: 03/07/2021
  */
 
-#include "contactlist.cpp"
 #include "contact.hpp"
+#include "contactlist.cpp"
 #include <iostream>
 using namespace std;
 
-int main() {
-    
-    PhoneBook ourPhoneBook; // Instantiation of PhoneBook object to contain contact names & phone numbers
+int main()
+{
+
+    PhoneBook ourPhoneBook; // Instantiation of PhoneBook object to contain
+                            // contact names & phone numbers
     char userSelection;     // Variable for use in user interface
-    Contact newContact;     // Buffer variable to contain new contact details to insert into list
+    Contact newContact;     // Buffer variable to contain new contact details to
+                            // insert into list
     string fName;
     string lName;
-    
+
     /*
      User interface prompts the user to insert a new contact, delete a contact,
-     or print the list of contacts. The appropriate methods are called based on the users' selection,
-     and the prompt repeats until the user chooses to quit.
+     or print the list of contacts. The appropriate methods are called based on
+     the users' selection, and the prompt repeats until the user chooses to
+     quit.
      */
     bool quit = false;
-    
-    while (!quit) {
-        
+
+    while (!quit)
+    {
+
         cout << "Choose from the following options: \n";
         cout << "i - insert new contact\n";
         cout << "d - delete a contact\n";
         cout << "p - print list of contacts\n";
         cout << "q - quit\n";
-    
+
         cin >> userSelection;
-    
-        switch (userSelection) {
-        
-            // FIXED: 'insert new contact' selection now passes initialized Contact to insert function
-            case 'i':
-                cout << "Enter contact name to insert: ";
-                cin.ignore();
-                cin >> newContact.firstName;
-                cin.ignore();
-                cin >> newContact.lastName;
-                cout << "Enter contact phone number: ";
-                cin >> newContact.tel;
-                ourPhoneBook.insert(newContact);
-                
-                break;
-                
-                // FIXED: 'delete' selection now passes first and last name to remove function
-            case 'd':
-                cout << "Enter contact name to remove: ";
-                cin.ignore();
-                cin >> fName;
-                cin.ignore();
-                cin >> lName;
-                ourPhoneBook.remove(fName, lName);
-                break;
-                
-            case 'p':
-                ourPhoneBook.print();
-                break;
-                
-            case 'q':
-                quit = true;
-                break;
-                
-            default:
-                cout << "Invalid selection. Try again.\n";
-                break;
+
+        switch (userSelection)
+        {
+
+        // FIXED: 'insert new contact' selection now passes initialized Contact
+        // to insert function
+        case 'i':
+            cout << "Enter contact name to insert: ";
+            cin.ignore();
+            cin >> newContact.firstName;
+            cin.ignore();
+            cin >> newContact.lastName;
+            cout << "Enter contact phone number: ";
+            cin >> newContact.tel;
+            ourPhoneBook.insert(newContact);
+
+            break;
+
+            // FIXED: 'delete' selection now passes first and last name to
+            // remove function
+        case 'd':
+            cout << "Enter contact name to remove: ";
+            cin.ignore();
+            cin >> newContact.firstName;
+            cin.ignore();
+            cin >> newContact.lastName;
+            cout << "Enter contact phone number: ";
+            cin >> newContact.tel;
+            ourPhoneBook.remove(newContact);
+            break;
+
+        case 'p':
+            ourPhoneBook.print();
+            break;
+
+        case 'q':
+            quit = true;
+            break;
+
+        default:
+            cout << "Invalid selection. Try again.\n";
+            break;
         }
     }
     return 0;
